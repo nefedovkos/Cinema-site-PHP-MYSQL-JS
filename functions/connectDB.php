b60798bad277526e28b887e35adddb925ea242cd
Konstantin Nefedov <60386908+nefedovkos@users.noreply.github.com>
2020-10-11 16:32:08 +0300

    Add files via upload

--- functions/connectDB.php
+++ functions/connectDB.php
@@ -1,6 +1,6 @@
 <?php
 
-  $connection_db = mysqli_connect('localhost','root','root','cinemaDB');
+  $connection_db = mysqli_connect('eu-cdbr-west-03.cleardb.net:3306','b52b000f6f17e5','dec7c083','heroku_303afcb7eb079bc');
   if(mysqli_connect_errno()){
     echo 'Error to connection DB ('.mysqli_connect_errno().'): '.mysqli_connect_error();
     exit();
